73e1b35bb35b6a21462190498da477b7820b77b8
Poli Gonzalez <gonzalezpoli10@gmail.com>
2023-11-16 10:06:08 -0600

    Refactored tests up to exercise 10

--- code.js
+++ code.js
@@ -3,5 +3,16 @@ function helloWorld() {
     return "Hello, World!";
 }
 function sayHello(input) {
-   return `Hello, ${input}!`
+    if(input === true) {
+        return `Hello, World!`
+    }
+    if(input) {
+        return `Hello, ${input}!`
+    }
+    return 'Hello, World!'
+}
+function isFive(input) {
+    if(input === 5) {
+        return true
+    }
 }
--- tests.js
+++ tests.js
@@ -17,13 +17,27 @@ describe('sayHello', function() {
     it('should be a defined function', function() {
         expect(typeof sayHello).toBe('function');
     });
-    it('should return a string when called', function() {
+    it('should return "Hello, Jane!" when passed "Jane"', function() {
         expect(sayHello('Jane')).toBe('Hello, Jane!');
     });
-    it('should return a string when called', function() {
+    it('should return "Hello, Alex!" when passed "Alex"', function() {
         expect(sayHello('Alex')).toBe('Hello, Alex!');
     });
-    it('should return a string when called', function() {
+    it('should return "Hello, Pat!" when passed "Pat"', function() {
         expect(sayHello('Pat')).toBe('Hello, Pat!');
+    });
+    it('should return "Hello, World!" when called', function() {
+        expect(sayHello()).toBe('Hello, World!');
+    });
+    it('should return "Hello, World!" when passed "true"', function() {
+        expect(sayHello(true)).toBe('Hello, World!');
+    });
+    it('should return "Hello, World!" when passed "false"', function() {
+        expect(sayHello(false)).toBe('Hello, World!');
+    });
+});
+describe('isFive', function() {
+    it('should be a defined function', function() {
+        expect(typeof isFive).toBe('function');
     });
 });
